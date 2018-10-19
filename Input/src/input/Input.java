5b23b8f146af74eff04bbedaca0db05058f98a5f
mcclejas <mcclejas@011-4303-S27MC.students.cisd.conroeisd.loc>
2018-10-19 07:50:05 -0500

    yeah...

--- Input/src/input/Input.java
+++ Input/src/input/Input.java
@@ -36,7 +36,7 @@ public class Input {
           out.printf("%s %s %s %s %s", stringArr[0], stringArr[1], stringArr[2], stringArr[3], stringArr[4]);
           
           
-          
+          out.println("i changed things");
           
           
      }
